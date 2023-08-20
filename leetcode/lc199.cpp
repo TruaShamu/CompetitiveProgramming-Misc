5f9d6d52f3b73fdbb1d98949596ac04098730e18
Trua Shamu <sofia_mit@hotmail.com>
2023-08-20 09:19:16 -0500

    Create lc199.cpp

---
+++ leetcode/lc199.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> ret;
+    vector<int> rightSideView(TreeNode* root) {
+        dfs(root, 0);
+        return ret; 
+    }
+    void dfs(TreeNode* node, int depth) {
+        if (!node) {
+            return;
+        }
+        if (ret.size() <= depth) {
+            ret.push_back(node->val);
+        }
+        dfs(node->right, depth+1);
+        dfs(node->left, depth+1);
+    }
+};
